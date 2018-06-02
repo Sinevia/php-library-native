af26a4c9b58eefe8cb68c0a706e15e0e764bf398
Sinevia <info@sinevia.com>
2018-06-02 04:49:16 +0100

    Update Os.php

--- src/Os.php
+++ src/Os.php
@@ -1,1 +1,14 @@
-
+<?php
+// ========================================================================= //
+// SINEVIA CONFIDENTIAL                                  http://sinevia.com  //
+// ------------------------------------------------------------------------- //
+// COPYRIGHT (c) 2018 Sinevia Ltd                   All rights reserved //
+// ------------------------------------------------------------------------- //
+// LICENCE: All information contained herein is, and remains, property of    //
+// Sinevia Ltd at all times.  Any intellectual and technical concepts        //
+// are proprietary to Sinevia Ltd and may be covered by existing patents,    //
+// patents in process, and are protected by trade secret or copyright law.   //
+// Dissemination or reproduction of this information is strictly forbidden   //
+// unless prior written permission is obtained from Sinevia Ltd per domain.  //
+//===========================================================================//
+namespace Sinevia;
