aa8cd6db05d69d804491c605ab282b0e99e91942
Sinevia <info@sinevia.com>
2019-09-01 23:43:39 +0100

    Update Os.php

--- src/Os.php
+++ src/Os.php
@@ -12,3 +12,38 @@
 // unless prior written permission is obtained from Sinevia Ltd per domain.  //
 //===========================================================================//
 namespace Sinevia;
+
+class Os {
+    public static function directoryMergeRecursive($sourceDir, $destinationDir) {
+        if (self::isWindows()) {
+            return self::directoryMergeRecursiveWindows($sourceDir, $destinationDir);
+        } else {
+            return self::directoryMergeRecursiveLinux($sourceDir, $destinationDir);
+        }
+    }
+
+    public static function directoryDeleteRecursive($sourceDir) {
+        if (self::isWindows()) {
+            return self::directoryDeleteRecursiveWindows($sourceDir);
+        } else {
+            return self::directoryDeleteRecursiveLinux($sourceDir);
+        }
+    }
+  
+    private static function directoryDeleteRecursiveLinux($sourceDir){
+        $cmd = 'rm -rf ' . $sourceDir . '"';
+        return self::exec($cmd);
+    }
+  
+    private static function directoryDeleteRecursiveWindows($sourceDir){
+        $sourceDirPathFixed = str_replace('/', DIRECTORY_SEPARATOR, $sourceDir);
+        $cmd = 'rmdir "' . $sourceDirPathFixed . '" /s /q';
+        return self::exec($cmd);
+    }
+  
+    private static function directoryMergeRecursiveLinux($sourceDir, $destinationDir){
+    }
+  
+    private static function directoryMergeRecursiveWindows($sourceDir, $destinationDir){
+    }
+}
